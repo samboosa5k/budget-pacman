9d67d24ae91f6a1aa4a286d54bb48f3ecb7f9d0c
samboosa5k <43879542+samboosa5k@users.noreply.github.com>
2019-10-04 12:23:40 +0200

    fixed index

--- src/js/index.js
+++ src/js/index.js
@@ -10,36 +10,28 @@ const app = document.getElementById( 'app' );
 
 
 document.addEventListener( 'DOMContentLoaded', () => {
-  const app = document.querySelector( '#app' );
+  const app = document.querySelector( '#app' )
   const stage = new Stage( 10, 10 );
-
   stage.mount( app );
-
   const stageArea = document.querySelector( '.stage' );
-  const newPacman = new Pacman( 85, 0, 0 );
 
+  const wall = new Entity( 1, 1, 'wall' );
+  const apple = new Entity( 2, 2, 'apple' );
+  const bomb = new Entity( 3, 3, 'bomb' );
+  wall.mount( stageArea );
+  stage.addEntity( wall );
+
+  apple.mount( stageArea );
+  stage.addEntity( apple );
+
+  bomb.mount( stageArea );
+  stage.addEntity( bomb );
+
+  const newPacman = new Pacman( 170, 170, 170 );
   newPacman.mount( stageArea );
-  console.log( stageArea );
+
+  console.log( stage.entities )
+  console.log( stage.collisionDetection( 1, 1 ) )
+  console.log( stage.collisionDetection( 2, 2 ) )
+
 } );
-  const app = document.querySelector('#app')
-  const stage = new Stage(10, 10);
-  stage.mount(app);
-  const stageArea = document.querySelector('.stage'); 
-
-  const wall = new Entity(1,1,'wall');
-  const apple = new Entity(2,2,'apple');
-  const bomb = new Entity(3,3,'bomb');
-  wall.mount(stageArea);
-  stage.addEntity(wall);
-  
-  apple.mount(stageArea);
-  stage.addEntity(apple);
-
-  bomb.mount(stageArea);
-  stage.addEntity(bomb);
-
-  console.log(stage.entities)
-  console.log(stage.collisionDetection(1,1))
-  console.log(stage.collisionDetection(2,2))
-
-});
