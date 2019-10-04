29e43ba69356a7c4f8985fcd316ccab46dbad6dd
samboosa5k <43879542+samboosa5k@users.noreply.github.com>
2019-10-04 10:10:52 +0200

    Basic left and right movement, and moouth movement, refactoring needed, help me

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,51 @@
+//  Constants
+const TILE_SIZE = 85;
+
+
+
 document.addEventListener( 'DOMContentLoaded', () => {
+  //  Player selction
   const player = document.getElementById( 'player' );
 
+  //  Player position
+  let xpos = player.offsetLeft;
+
   //  Click player mouth open
-  document.addEventListener( 'mousedown', () => {
-    console.log( 'Mouth open' );
-    player.style.backgroundPositionX = '170px';
+  document.addEventListener( 'keydown', ( event ) => {
+    if ( event.code === 'ArrowRight' ) {
+
+      //  Mouth opens
+      console.log( 'Mouth open' );
+      player.style.backgroundPositionX = '170px';
+
+      //  Player moves
+      console.log( 'Player is moving right' );
+      xpos = xpos + TILE_SIZE;
+      player.style.left = `${xpos}px`;
+      console.log( xpos );
+    }
+
+    if ( event.code === 'ArrowLeft' ) {
+      //  Mouth opens
+      console.log( 'Mouth open' );
+      player.style.backgroundPositionX = '170px';
+
+      //  Player moves
+      console.log( 'Player is moving left' );
+      xpos = xpos - TILE_SIZE;
+      player.style.left = `${xpos}px`;
+      console.log( xpos );
+    }
   } );
 
   //  Click reset player mouth close
-  document.addEventListener( 'mouseup', () => {
-    console.log( 'Mouth closed' );
-    player.style.backgroundPositionX = '85px';
+  document.addEventListener( 'keyup', ( event ) => {
+    if ( event.code === 'ArrowRight' || event.code === 'ArrowLeft' ) {
+
+      //  Mouth closes
+      console.log( 'Mouth closed' );
+      player.style.backgroundPositionX = '85px';
+    }
   } );
 
 
